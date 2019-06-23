0af489e2d7b85e6a663d1bb8ef8dfb6c66673023
Drew Seeholzer <drews21f7@gmail.com>
2019-06-23 17:31:38 -0600

    Made custom ButtonTableViewCell

---
+++ Assessment2take2/View Controllers/ButtonTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  ButtonTableViewCell.swift
+//  Assessment2take2
+//
+//  Created by Drew Seeholzer on 6/23/19.
+//  Copyright © 2019 Drew Seeholzer. All rights reserved.
+//
+
+import UIKit
+
+protocol ButtonTableViewCellDelegate {
+    func buttonCellButtonTapped (_ sender: ButtonTableViewCell)
+}
+
+class ButtonTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var itemLabel: UILabel!
+    
+    @IBOutlet weak var checkedButton: UIButton!
+    
+    var delegate: ButtonTableViewCellDelegate?
+    
+    fileprivate func updateButton(_ isChecked: Bool) {
+        let imageName = isChecked ? "complete" : "incomplete"
+        checkedButton.setImage(UIImage(named: imageName), for: .normal)
+    }
+    
+    func update(withItem item: List) {
+        itemLabel.text = item.item
+        updateButton(item.isChecked)
+    }
+    
+    @IBAction func checkedButtonTapped(_ sender: Any) {
+    }
+}
